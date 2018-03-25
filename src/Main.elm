47bc84f5549b5df725b177213e6440896ac514d3
Chadtech <chadstearns1@gmail.com>
2018-03-25 17:03:55 +0200

    Bringing main back with proper capitalization

---
+++ src/Main.elm
@@ -0,0 +1,72 @@
+module Main exposing (..)
+
+import Cmd.Extra exposing (withCmds, withNoCmd)
+import Html
+import Html.Styled exposing (toUnstyled)
+import Model exposing (Model)
+import Msg exposing (Msg(..))
+import Ports exposing (JsMsg(ConsoleLog, Square))
+import View exposing (view)
+
+
+-- MAIN --
+
+
+main : Program Never Model Msg
+main =
+    { init = init
+    , view = toUnstyled << view
+    , update = update
+    , subscriptions = subscriptions
+    }
+        |> Html.program
+
+
+init : ( Model, Cmd Msg )
+init =
+    { field = ""
+    , timesEnterWasPressed = 0
+    , squareOfEnterPresses = 0
+    }
+        |> withNoCmd
+
+
+
+-- SUBSCRIPTIONS --
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Ports.fromJs Msg.decode
+
+
+
+-- UPDATE --
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        UpdateField str ->
+            { model | field = str }
+                |> withNoCmd
+
+        EnterHappened ->
+            let
+                newCount =
+                    model.timesEnterWasPressed + 1
+            in
+            { model
+                | timesEnterWasPressed = newCount
+            }
+                |> withCmds
+                    [ Ports.send (ConsoleLog model.field)
+                    , Ports.send (Square newCount)
+                    ]
+
+        ReceivedSquare square ->
+            { model | squareOfEnterPresses = square }
+                |> withNoCmd
+
+        MsgDecodeFailed _ ->
+            model |> withNoCmd
